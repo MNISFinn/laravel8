3f21931a074880954a1fccc9bc4b599511526f5f
MNISFinn <957810425@qq.com>
2021-08-16 23:00:38 +0800

    登录接口暂时在api路由下访问

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\BuyController;
 use App\Http\Controllers\api\Buy2Controller;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,3 +28,6 @@ Route::group(['middleware' => 'login'],function () {
 //    Route::get('/buy_test2', [Buy2Controller::class, 'test'])->withoutMiddleware('login');
     Route::get('/buy_test2', [Buy2Controller::class, 'test']);
 });
+
+Route::get('/login', [LoginController::class, 'login']);
+Route::get('/get_permission', [LoginController::class, 'get_permission']);
