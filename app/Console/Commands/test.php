9daefb320a6cf734032b8c1736b55035a2e0388c
MNISFinn <957810425@qq.com>
2022-05-04 13:42:55 +0800

    test command

---
+++ app/Console/Commands/test.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'k:start';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle() {
+        $num = 0;
+        do {
+            $num ++;
+            echo $num;
+            Log::info($num);
+        } while (true);
+    }
+}
